aa1447543341138c2d75d2268140f5129f1fa197
KatyaYermolayeva <50108213+KatyaYermolayeva@users.noreply.github.com>
2022-09-30 21:13:53 +0300

    Update HelloWorldController.cs

--- HelloWorld/HelloWorld/Controllers/HelloWorldController.cs
+++ HelloWorld/HelloWorld/Controllers/HelloWorldController.cs
@@ -3,14 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace HelloWorld.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class HelloWorldController : ControllerBase
     {
         public HelloWorldController()
         {
         }
 
-        [HttpGet(Name = "GetHelloWorld")]
         public String Get()
         {
             return "Hello, World!";
